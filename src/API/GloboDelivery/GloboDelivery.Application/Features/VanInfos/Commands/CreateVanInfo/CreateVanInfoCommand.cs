d648a88e9b8309f7605a1060265e6e36456f08b8
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 21:47:37 +0300

    Add CreateVanInfoCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Commands/CreateVanInfo/CreateVanInfoCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Commands.CreateVanInfo
+{
+    public class CreateVanInfoCommand : IRequest<CreateVanInfoDto>
+    {
+        public string Mark { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public decimal Capacity { get; set; }
+        public DateTime LastInspectionDate { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
