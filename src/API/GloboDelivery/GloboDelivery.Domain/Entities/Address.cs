ede19524431fc71eb81ae0aecd109469b9fd6740
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 19:54:37 +0300

    Add Address entity

---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace GloboDelivery.Domain.Entities
+{
+    public class Address : BaseEntity
+    {
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string AdministrativeArea { get; set; } = string.Empty; 
+        public string AddressLine { get; set; } = string.Empty;
+        public int SuiteNumber { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
+    }
+}
