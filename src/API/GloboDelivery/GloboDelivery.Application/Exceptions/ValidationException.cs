3c97a498a3c6f12c25cd4261ccc621b105908cd0
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 22:18:16 +0300

    Create exception for validation errors

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace GloboDelivery.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string>? ValidationErrors { get; set; }
+        public ValidationException() { }
+        public ValidationException(string message) : base(message) { }
+        public ValidationException(string message, Exception inner) : base(message, inner) { }
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = new List<string>();
+
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidationErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
