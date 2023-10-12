97e3bcf24feab1ae22608524d2bcc9d98ac86344
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 21:46:21 +0300

    Add CreateVanInfoDto model

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Commands/CreateVanInfo/CreateVanInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace GloboDelivery.Application.Features.VanInfos.Commands.CreateVanInfo
+{
+    public class CreateVanInfoDto
+    {
+        public int Id { get; set; }
+        public string Mark { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public decimal Capacity { get; set; }
+        public DateTime LastInspectionDate { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
