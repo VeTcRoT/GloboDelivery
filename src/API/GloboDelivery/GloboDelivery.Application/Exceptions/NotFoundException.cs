a07e8e201ea778b428f11cb47a42bae3c66bcb45
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 21:15:02 +0300

    Add NotFound exception

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace GloboDelivery.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() { }
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, Exception inner) : base(message, inner) { }
+        public NotFoundException(string name, object key)
+           : base($"{name} ({key}) is not found") { }
+    }
+}
