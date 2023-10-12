732006573d4f524acb09c636a28677d0806a0623
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-12 19:53:24 +0300

    Add Base and Delivery entities

---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Entities/Delivery.cs
@@ -0,0 +1,11 @@
+﻿namespace GloboDelivery.Domain.Entities
+{
+    public class Delivery : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public bool IsAvailable { get; set; }
+        public int VanInfoId { get; set; }
+    }
+}
