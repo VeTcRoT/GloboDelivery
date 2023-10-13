4e0cfaa854656467188a0d46b96dfed1b2ceb845
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 19:51:26 +0300

    Add AddressProfile

---
+++ src/API/GloboDelivery/GloboDelivery.Application/MappingProfiles/AddressProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GloboDelivery.Application.Features.Addresses.CreateAddress;
+using GloboDelivery.Domain.Entities;
+using GloboDelivery.Domain.Dtos;
+
+namespace GloboDelivery.Application.MappingProfiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<CreateAddressCommand, Address>();
+            CreateMap<Address, AddressDto>();
+        }
+    }
+}
