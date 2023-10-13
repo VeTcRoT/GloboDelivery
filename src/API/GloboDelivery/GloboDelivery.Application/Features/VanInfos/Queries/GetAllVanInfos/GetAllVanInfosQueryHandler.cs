1b8550fbfee96208dd9cd8d053f4cafc9aebc4cb
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 19:20:55 +0300

    Implement GetAllVanInfosQueryHandler

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Queries/GetAllVanInfos/GetAllVanInfosQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Entities;
+using GloboDelivery.Domain.Interfaces;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Queries.GetAllVanInfos
+{
+    public class GetAllVanInfosQueryHandler : IRequestHandler<GetAllVanInfosQuery, List<VanInfoDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllVanInfosQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<VanInfoDto>> Handle(GetAllVanInfosQuery request, CancellationToken cancellationToken)
+        {
+            var vanInfos = await _unitOfWork.Repository<VanInfo>().ListAllAsync();
+
+            return _mapper.Map<VanInfoDto>(vanInfos);
+        }
+    }
+}
