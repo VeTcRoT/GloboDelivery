81f2d0ab6c9d65200e3efb4bb1d064c1671e9b2e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 19:06:24 +0300

    Create GetVanInfoByIdCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Queries/GetVanInfoById/GetVanInfoByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Queries.GetVanInfoById
+{
+    public class GetVanInfoByIdCommand : IRequest<GetVanInfoByIdDto>
+    {
+        public int Id { get; set; }
+    }
+}
