8191e4f73a78256d98ffb6096fb80fd4674fd644
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 18:40:28 +0300

    Add DeleteVanInfoCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Commands/DeleteVanInfo/DeleteVanInfoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Commands.DeleteVanInfo
+{
+    public class DeleteVanInfoCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
