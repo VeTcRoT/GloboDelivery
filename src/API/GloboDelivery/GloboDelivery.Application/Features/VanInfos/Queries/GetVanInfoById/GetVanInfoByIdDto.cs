6530687afda1e9aff65dd6c2e55ee6176da29727
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 19:05:21 +0300

    Add GetVanInfoByIdDto

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Queries/GetVanInfoById/GetVanInfoByIdDto.cs
@@ -0,0 +1,14 @@
+﻿namespace GloboDelivery.Application.Features.VanInfos.Queries.GetVanInfoById
+{
+    public class GetVanInfoByIdDto
+    {
+        public int Id { get; set; }
+        public string Mark { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public decimal Capacity { get; set; }
+        public DateTime LastInspectionDate { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
