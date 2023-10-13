05556d8ed06eac5007474b5ed5da991f012cd972
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 20:05:37 +0300

    Add DeleteAddressCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Addresses/DeleteAddress/DeleteAddressCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.Addresses.DeleteAddress
+{
+    public class DeleteAddressCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
