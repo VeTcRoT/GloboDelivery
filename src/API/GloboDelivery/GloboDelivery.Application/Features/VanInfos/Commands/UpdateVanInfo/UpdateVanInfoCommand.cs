a820b8d59fe759e6c5da3a4a663002031acae9e6
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 18:27:32 +0300

    Add UpdateVanInfoCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Commands/UpdateVanInfo/UpdateVanInfoCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Commands.UpdateVanInfo
+{
+    public class UpdateVanInfoCommand : IRequest
+    {
+        public string Mark { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public decimal Capacity { get; set; }
+        public DateTime LastInspectionDate { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
