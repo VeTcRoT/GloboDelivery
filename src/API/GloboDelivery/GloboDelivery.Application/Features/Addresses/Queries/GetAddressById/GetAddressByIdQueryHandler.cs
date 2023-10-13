521ce5d8b0203f3930fb3f14d45767c10b7c675e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 20:21:20 +0300

    Implement GetAddressByIdQueryHandler

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Addresses/Queries/GetAddressById/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GloboDelivery.Application.Exceptions;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Entities;
+using GloboDelivery.Domain.Interfaces;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Addresses.Queries.GetAddressById
+{
+    public class GetAddressByIdQueryHandler : IRequestHandler<GetAddressByIdQuery, AddressDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAddressByIdQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<AddressDto> Handle(GetAddressByIdQuery request, CancellationToken cancellationToken)
+        {
+            var address = await _unitOfWork.Repository<Address>().GetByIdAsync(request.Id);
+
+            if (address == null)
+                throw new NotFoundException(nameof(Address), request.Id);
+
+            return _mapper.Map<AddressDto>(address);
+        }
+    }
+}
