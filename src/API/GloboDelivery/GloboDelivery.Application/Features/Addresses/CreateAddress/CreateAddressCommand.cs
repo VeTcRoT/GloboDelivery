7fe9748d1e5def28c9d89bfde80ba681a44a671a
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-13 19:37:54 +0300

    Create CreateAddressCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Addresses/CreateAddress/CreateAddressCommand.cs
@@ -0,0 +1,15 @@
+﻿using GloboDelivery.Domain.Dtos;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Addresses.CreateAddress
+{
+    public class CreateAddressCommand : IRequest<AddressDto>
+    {
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string AdministrativeArea { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public int SuiteNumber { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
+    }
+}
