2e7e688e1af1c418a3d1b03f0e3720c61b1e8916
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 16:33:06 +0300

    Add DeliveryListingDto model

---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Dtos/DeliveryListingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GloboDelivery.Domain.Dtos
+{
+    public class DeliveryListingDto
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal CapacityTaken { get; set; }
+    }
+}
