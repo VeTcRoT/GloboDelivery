5c5ac006f6c127004d5add121b5f236f53964307
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 10:48:23 +0300

    Add CreateDeliveryCommand

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Commands/CreateDeliveryCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace GloboDelivery.Application.Features.Deliveries.Commands
+{
+    public class CreateDeliveryCommand : IRequest
+    {
+        public decimal Price { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal CapacityTaken { get; set; }
+        public int VanInfoId { get; set; }
+        public IEnumerable<int> AddressesIds { get; set; } = new List<int>();
+    }
+}
