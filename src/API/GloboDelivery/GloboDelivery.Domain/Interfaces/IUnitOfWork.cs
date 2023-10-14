863c32269156596aa4fb704597f5f4f1ec3f57d0
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 10:43:16 +0300

    Add property to IUnitOfWork interface

--- src/API/GloboDelivery/GloboDelivery.Domain/Interfaces/IUnitOfWork.cs
+++ src/API/GloboDelivery/GloboDelivery.Domain/Interfaces/IUnitOfWork.cs
@@ -4,6 +4,7 @@ namespace GloboDelivery.Domain.Interfaces
 {
     public interface IUnitOfWork
     {
+        IAddressRepository AddressRepository { get; }
         IBaseRepository<T> Repository<T>() where T : BaseEntity;
         Task<int> SaveChangesAsync();
     }
