e78c8c9659b8b7c2749707fcdb4abab8462bdd90
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 16:43:28 +0300

    Implement GetAllDeliveriesQueryHandler

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Queries/GetAllDeliveries/GetAllDeliveriesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Interfaces;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Deliveries.Queries.GetAllDeliveries
+{
+    public class GetAllDeliveriesQueryHandler : IRequestHandler<GetAllDeliveriesQuery, IReadOnlyList<DeliveryListingDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllDeliveriesQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IReadOnlyList<DeliveryListingDto>> Handle(GetAllDeliveriesQuery request, CancellationToken cancellationToken)
+        {
+            var deliveries = await _unitOfWork.DeliveryRepository.ListAllAsync();
+
+            return _mapper.Map<IReadOnlyList<DeliveryListingDto>>(deliveries);
+        }
+    }
+}
