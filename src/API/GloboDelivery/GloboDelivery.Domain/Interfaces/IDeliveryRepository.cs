a225cc74723da10bfa93c827ed90ff89156c01a4
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 16:02:27 +0300

    Create IDeliveryRepository interface

---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Interfaces/IDeliveryRepository.cs
@@ -0,0 +1,9 @@
+﻿using GloboDelivery.Domain.Entities;
+
+namespace GloboDelivery.Domain.Interfaces
+{
+    public interface IDeliveryRepository : IBaseRepository<Delivery>
+    {
+        Task<Delivery?> GetByIdWithDeliveryAddressesAsync(int id);
+    }
+}
