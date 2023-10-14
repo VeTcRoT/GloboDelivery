2d91c51350119386b1198db15d1d62d6cb2eae10
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 16:30:18 +0300

    Add GetDeliveryByIdQuery

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Queries/GetDeliveryById/GetDeliveryByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using GloboDelivery.Domain.Dtos;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Deliveries.Queries.GetDeliveryById
+{
+    public class GetDeliveryByIdQuery : IRequest<DeliveryDto>
+    {
+        public int Id { get; set; }
+    }
+}
