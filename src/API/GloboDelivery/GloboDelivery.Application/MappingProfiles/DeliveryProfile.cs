108bfccdb8ae1b9edbf3bef365fa5805e37ab4d0
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 11:39:43 +0300

    Add Delivery mapping profile

---
+++ src/API/GloboDelivery/GloboDelivery.Application/MappingProfiles/DeliveryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GloboDelivery.Application.Features.Deliveries.Commands;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Entities;
+
+namespace GloboDelivery.Application.MappingProfiles
+{
+    public class DeliveryProfile : Profile
+    {
+        public DeliveryProfile()
+        {
+            CreateMap<CreateDeliveryCommand, Delivery>();
+            CreateMap<Delivery, DeliveryDto>();
+        }
+    }
+}
