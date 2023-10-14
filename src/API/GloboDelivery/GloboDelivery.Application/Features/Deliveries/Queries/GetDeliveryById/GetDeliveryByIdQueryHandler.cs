9997062a589fecc092cd873f76ed1580a9a1539d
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-14 16:37:12 +0300

    Implement GetDeliveryByIdQueryHandler

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Queries/GetDeliveryById/GetDeliveryByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GloboDelivery.Application.Exceptions;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Entities;
+using GloboDelivery.Domain.Interfaces;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Deliveries.Queries.GetDeliveryById
+{
+    public class GetDeliveryByIdQueryHandler : IRequestHandler<GetDeliveryByIdQuery, DeliveryListingDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetDeliveryByIdQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<DeliveryListingDto> Handle(GetDeliveryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var delivery = await _unitOfWork.DeliveryRepository.GetByIdAsync(request.Id);
+
+            if (delivery == null)
+                throw new NotFoundException(nameof(Delivery), request.Id);
+
+            return _mapper.Map<DeliveryListingDto>(delivery);
+        }
+    }
+}
