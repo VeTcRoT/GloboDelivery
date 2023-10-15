42661fb6a98ad336e85f59895ff8147d5e7eb4c6
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 11:42:02 +0300

    Add persistence service registration

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using GloboDelivery.Domain.Interfaces;
+using GloboDelivery.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboDelivery.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+
+            return services;
+        }
+    }
+}
