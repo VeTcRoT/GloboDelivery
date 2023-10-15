09092275949d3afda0dffe0907224fa31810e9a0
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 13:46:49 +0300

    Add UseCustomExceptionHandler extension method

---
+++ src/API/GloboDelivery/GloboDelivery.API/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace GloboDelivery.API.Middlewares
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
