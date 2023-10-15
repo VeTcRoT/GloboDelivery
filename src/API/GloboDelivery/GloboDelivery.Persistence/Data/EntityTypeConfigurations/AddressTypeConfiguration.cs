352e01a633cae086c11a7e8e6f169f6d5a008748
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 10:52:49 +0300

    Add entity type configuration for Address

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/Data/EntityTypeConfigurations/AddressTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GloboDelivery.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GloboDelivery.Persistence.Data.EntityTypeConfigurations
+{
+    public class AddressTypeConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.HasMany(a => a.DeliveryAddresses)
+                .WithOne(da => da.Address)
+                .HasForeignKey(da => da.AddressId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
