56bcc52adebcb53ea5fa8ee6ab461cd1c0010581
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 10:58:48 +0300

    Add entity type configuration for Delivery

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/Data/EntityTypeConfigurations/DeliveryTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GloboDelivery.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboDelivery.Persistence.Data.EntityTypeConfigurations
+{
+    public class DeliveryTypeConfiguration : IEntityTypeConfiguration<Delivery>
+    {
+        public void Configure(EntityTypeBuilder<Delivery> builder)
+        {
+            builder.HasKey(d => d.Id);
+
+            builder.HasMany(d => d.DeliveryAddresses)
+                .WithOne(da => da.Delivery)
+                .HasForeignKey(da => da.DeliveryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
