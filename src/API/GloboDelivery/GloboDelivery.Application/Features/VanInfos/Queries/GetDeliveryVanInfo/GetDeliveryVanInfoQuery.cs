ab202854e44d35c00557c61f737c1cb4c836025c
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 20:25:41 +0300

    Implement GetDeliveryVanInfo query

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/VanInfos/Queries/GetDeliveryVanInfo/GetDeliveryVanInfoQuery.cs
@@ -0,0 +1,10 @@
+﻿using GloboDelivery.Domain.Dtos;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.VanInfos.Queries.GetDeliveryVanInfo
+{
+    public class GetDeliveryVanInfoQuery : IRequest<VanInfoDto>
+    {
+        public int DeliveryId { get; set; }
+    }
+}
