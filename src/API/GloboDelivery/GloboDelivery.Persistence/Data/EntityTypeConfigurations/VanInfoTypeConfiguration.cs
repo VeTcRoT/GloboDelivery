5b9b9062ee762dc091ab26344f1aec15f49586e3
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 10:59:00 +0300

    Add entity type configuration for VanInfo

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/Data/EntityTypeConfigurations/VanInfoTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GloboDelivery.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboDelivery.Persistence.Data.EntityTypeConfigurations
+{
+    public class VanInfoTypeConfiguration : IEntityTypeConfiguration<VanInfo>
+    {
+        public void Configure(EntityTypeBuilder<VanInfo> builder)
+        {
+            builder.HasKey(vi => vi.Id);
+
+            builder.HasMany(vi => vi.Deliveries)
+                .WithOne(d => d.VanInfo)
+                .HasForeignKey(d => d.VanInfoId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
