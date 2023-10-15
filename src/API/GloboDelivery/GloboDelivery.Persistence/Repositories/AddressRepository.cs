d4f1e3592c8ad8e7806f8f331a2cc5d41b3fba56
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 11:25:01 +0300

    Implement Address Repository

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/Repositories/AddressRepository.cs
@@ -0,0 +1,17 @@
+﻿using GloboDelivery.Domain.Entities;
+using GloboDelivery.Domain.Interfaces;
+using GloboDelivery.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboDelivery.Persistence.Repositories
+{
+    public class AddressRepository : BaseRepository<Address>, IAddressRepository
+    {
+        public AddressRepository(GloboDeliveryDbContext dbContext) : base(dbContext) { }
+
+        public async Task<IReadOnlyList<Address>> GetAddressesByIdsAsync(IEnumerable<int> ids)
+        {
+            return await _dbSet.Where(address => ids.Contains(address.Id)).ToListAsync();
+        }
+    }
+}
