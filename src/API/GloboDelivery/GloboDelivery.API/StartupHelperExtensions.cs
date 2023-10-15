1b1c8e302daa8a5bb00abf80f42701c305641673
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-15 13:27:56 +0300

    Setup services and pipeline

---
+++ src/API/GloboDelivery/GloboDelivery.API/StartupHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿using GloboDelivery.Application;
+using GloboDelivery.Persistence;
+using GloboDelivery.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboDelivery.API
+{
+    internal static class StartupHelperExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices();
+
+            builder.Services.AddDbContext<GloboDeliveryDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder.Build();
+        }
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
