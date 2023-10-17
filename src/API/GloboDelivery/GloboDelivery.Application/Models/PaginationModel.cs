21b0e9e0c94a1f4044f0f0306936bb4376e7056d
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-17 21:08:28 +0300

    PaginationModel improvements

--- src/API/GloboDelivery/GloboDelivery.Application/Models/PaginationModel.cs
+++ src/API/GloboDelivery/GloboDelivery.Application/Models/PaginationModel.cs
@@ -2,13 +2,18 @@
 {
     public class PaginationModel
     {
-        public int PageNumber { get; set; } = 1;
+        private int _pageNumber = 1;
+        public int PageNumber 
+        { 
+            get => _pageNumber; 
+            set => _pageNumber = value <= 0 ? 1 : value; 
+        }
 
         private int _pageSize = 10;
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > 10) ? 10 : value;
+            set => _pageSize = (value > 10 || value <= 0) ? 10 : value;
         }
     }
 }
