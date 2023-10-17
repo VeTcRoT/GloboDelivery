26ca6b988a6ceaff09b989ec8d0bf4376cf2eac9
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-17 21:24:09 +0300

    ValidationException adjustments

--- src/API/GloboDelivery/GloboDelivery.Application/Exceptions/ValidationException.cs
+++ src/API/GloboDelivery/GloboDelivery.Application/Exceptions/ValidationException.cs
@@ -4,18 +4,15 @@ namespace GloboDelivery.Application.Exceptions
 {
     public class ValidationException : Exception
     {
-        public List<string>? ValidationErrors { get; set; }
+        public Dictionary<string, string[]>? ValidationErrors { get; set; }
         public ValidationException() { }
         public ValidationException(string message) : base(message) { }
         public ValidationException(string message, Exception inner) : base(message, inner) { }
         public ValidationException(ValidationResult validationResult)
         {
-            ValidationErrors = new List<string>();
-
-            foreach (var validationError in validationResult.Errors)
-            {
-                ValidationErrors.Add(validationError.ErrorMessage);
-            }
+            ValidationErrors = validationResult.Errors
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
         }
     }
 }
