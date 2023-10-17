c994e6760806b0dffc5d8c6f08c1e3809f658ee2
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-17 19:15:56 +0300

    Add pagination model

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Models/PaginationModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GloboDelivery.Application.Models
+{
+    public class PaginationModel
+    {
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > 10) ? 10 : value;
+        }
+    }
+}
