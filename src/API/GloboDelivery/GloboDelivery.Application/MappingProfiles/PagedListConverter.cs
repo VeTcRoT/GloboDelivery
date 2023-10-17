16976808744b24d8bd207f3dc4b4f80813d536b7
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-17 20:41:47 +0300

    Add PagedListConverter for AutoMapper

---
+++ src/API/GloboDelivery/GloboDelivery.Application/MappingProfiles/PagedListConverter.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GloboDelivery.Domain.Helpers;
+
+namespace GloboDelivery.Application.MappingProfiles
+{
+    public class PagedListConverter<TSource, TDestination> : ITypeConverter<PagedList<TSource>, PagedList<TDestination>>
+    {
+        public PagedList<TDestination> Convert(PagedList<TSource> source, PagedList<TDestination> destination, ResolutionContext context)
+        {
+            var items = context.Mapper.Map<List<TDestination>>(source);
+            return new PagedList<TDestination>(items, source.TotalCount, source.CurrentPage, source.PageSize);
+        }
+    }
+}
