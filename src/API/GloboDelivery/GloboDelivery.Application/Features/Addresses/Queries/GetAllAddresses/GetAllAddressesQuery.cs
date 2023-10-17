c78fb4bbfe6938f5feab788a348a253c516fe009
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-17 20:40:28 +0300

    Add support for pagination at GetAllAddressesQuery

--- src/API/GloboDelivery/GloboDelivery.Application/Features/Addresses/Queries/GetAllAddresses/GetAllAddressesQuery.cs
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Addresses/Queries/GetAllAddresses/GetAllAddressesQuery.cs
@@ -1,10 +1,11 @@
 ﻿using GloboDelivery.Application.Models;
 using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Helpers;
 using MediatR;
 
 namespace GloboDelivery.Application.Features.Addresses.Queries.GetAllAddresses
 {
-    public class GetAllAddressesQuery : PaginationModel, IRequest<IReadOnlyList<AddressDto>>
+    public class GetAllAddressesQuery : PaginationModel, IRequest<PagedList<AddressDto>>
     {
     }
 }
