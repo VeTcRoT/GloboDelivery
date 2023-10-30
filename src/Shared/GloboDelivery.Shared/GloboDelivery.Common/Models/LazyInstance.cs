f0be11f961f03d48ff3cc8d625c87354632f6a88
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-30 18:06:52 +0200

    Add shared assembly and put LazyInstance class to it

---
+++ src/Shared/GloboDelivery.Shared/GloboDelivery.Common/Models/LazyInstance.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboDelivery.Common.Models
+{
+    public class LazyInstance<T> : Lazy<T>
+    {
+        public LazyInstance(IServiceProvider serviceProvider)
+            : base(() => serviceProvider.GetRequiredService<T>())
+        {
+
+        }
+    }
+}
