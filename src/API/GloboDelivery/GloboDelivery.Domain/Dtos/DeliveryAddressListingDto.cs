1ce16e5787d2f5a06b8142a7f40ead93fbcf82e5
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:49:13 +0200

    Add DeliveryAddressListingDto and DeliveryAddressManipulationDto models

---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Dtos/DeliveryAddressListingDto.cs
@@ -0,0 +1,15 @@
+﻿namespace GloboDelivery.Domain.Dtos
+{
+    public class DeliveryAddressListingDto
+    {
+        public int Id { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string AdministrativeArea { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public int SuiteNumber { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+    }
+}
---
+++ src/API/GloboDelivery/GloboDelivery.Domain/Dtos/DeliveryAddressManipulationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GloboDelivery.Domain.Dtos
+{
+    public class DeliveryAddressManipulationDto
+    {
+        public int AddressId { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+    }
+}
