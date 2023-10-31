4771396e1ac6ab04ac7d49e2cfc95dbd60b67ff3
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:56:33 +0200

    Adjust UpdateDelivery command

--- src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Commands/UpdateDelivery/UpdateDeliveryCommandValidator.cs
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Commands/UpdateDelivery/UpdateDeliveryCommandValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using GloboDelivery.Application.Exceptions;
+using GloboDelivery.Application.Features.Deliveries.Commands.CreateDelivery;
+using GloboDelivery.Domain.Dtos;
 using GloboDelivery.Domain.Entities;
 using GloboDelivery.Domain.Interfaces;
 
@@ -18,20 +20,35 @@ namespace GloboDelivery.Application.Features.Deliveries.Commands.UpdateDelivery
             RuleFor(d => d.CapacityTaken)
                 .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} should be greater of equal to 0.");
 
-            RuleFor(d => d.DepartureDate)
-                .GreaterThanOrEqualTo(DateTime.Now).WithMessage("{PropertyName} should be greater or equal to " + DateTime.Now);
-
-            RuleFor(d => d.AddressesIds)
-                .Must(ai => ai.Count() <= 5).WithMessage("{PropertyName} should not exceed a maximum length of 5 elements.");
+            RuleFor(d => d.AddressesDates)
+                .Must(ai => ai.Count() <= 5).WithMessage("{PropertyName} should not exceed a maximum length of 5 elements.")
+                .Must(DepartureDatesShouldBeGreaterThanCurrentDate).WithMessage("Departure date should be greater or equal to current datetime.")
+                .Must(ArrivalDatesShouldBeGreaterThanArrivalDates).WithMessage("Arrival date should be greater than departure date.");
 
             RuleFor(d => d)
-                .Must(ArrivalDateGreaterThanDepartureDate).WithMessage("ArrivalDate should be greater than DepartureDate.")
                 .MustAsync(CapacityTakenLessOrEqualToVanCapacity).WithMessage("CapacityTaken should be less or equal to Capacity of Van.");
         }
 
-        private bool ArrivalDateGreaterThanDepartureDate(UpdateDeliveryCommand command)
+        private bool DepartureDatesShouldBeGreaterThanCurrentDate(IEnumerable<DeliveryAddressManipulationDto> addressDates)
         {
-            return command.ArrivalDate > command.DepartureDate;
+            foreach (var addressDate in addressDates)
+            {
+                if (addressDate.DepartureDate < DateTime.Now)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool ArrivalDatesShouldBeGreaterThanArrivalDates(IEnumerable<DeliveryAddressManipulationDto> addressDates)
+        {
+            foreach (var addressDate in addressDates)
+            {
+                if (addressDate.DepartureDate >= addressDate.ArrivalDate)
+                    return false;
+            }
+
+            return true;
         }
 
         private async Task<bool> CapacityTakenLessOrEqualToVanCapacity(UpdateDeliveryCommand command, CancellationToken cancellationToken)
