32575c1ec77fc114e7c9a25fce7a49d73bf9e9d7
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:48:35 +0200

    Delete Departure and Arrival dates from DeliveryDto model

--- src/API/GloboDelivery/GloboDelivery.Domain/Dtos/DeliveryDto.cs
+++ src/API/GloboDelivery/GloboDelivery.Domain/Dtos/DeliveryDto.cs
@@ -4,8 +4,6 @@
     {
         public int Id { get; set; }
         public decimal Price { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public DateTime ArrivalDate { get; set; }
         public bool IsAvailable { get; set; }
         public decimal CapacityTaken { get; set; }
     }
