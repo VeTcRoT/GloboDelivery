a9d1a9f845c5ad93173b065e9b9a1783412b32f1
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:47:21 +0200

    Add migrations due to changes in entities

---
+++ src/API/GloboDelivery/GloboDelivery.Persistence/Migrations/20231030204314_AddDepartureAndArrivalDatesToDeliveryAddresses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GloboDelivery.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartureAndArrivalDatesToDeliveryAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivalDate",
+                table: "DeliveryAddresses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DepartureDate",
+                table: "DeliveryAddresses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArrivalDate",
+                table: "DeliveryAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "DepartureDate",
+                table: "DeliveryAddresses");
+        }
+    }
+}
