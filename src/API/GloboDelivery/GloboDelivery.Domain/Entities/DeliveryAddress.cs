a36ad46892f441a90ddeb37b89734726722822a9
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:46:41 +0200

    Add Departure and Arrival dates to DeliveryAddress entity

--- src/API/GloboDelivery/GloboDelivery.Domain/Entities/DeliveryAddress.cs
+++ src/API/GloboDelivery/GloboDelivery.Domain/Entities/DeliveryAddress.cs
@@ -4,6 +4,8 @@
     {
         public int DeliveryId { get; set; }
         public int AddressId { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
 
         public Delivery Delivery { get; set; } = null!;
         public Address Address { get; set; } = null!;
