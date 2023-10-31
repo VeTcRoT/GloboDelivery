b57004581a1c552d9c05b606089d93f6bbbcf7c8
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-31 18:46:09 +0200

    Delete Departure and Arrival dates from Delivery entity

--- src/API/GloboDelivery/GloboDelivery.Domain/Entities/Delivery.cs
+++ src/API/GloboDelivery/GloboDelivery.Domain/Entities/Delivery.cs
@@ -3,8 +3,6 @@
     public class Delivery : BaseEntity
     {
         public decimal Price { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public DateTime ArrivalDate { get; set; }
         public bool IsAvailable { get; set; }
         public decimal CapacityTaken { get; set; }
         public int VanInfoId { get; set; }
