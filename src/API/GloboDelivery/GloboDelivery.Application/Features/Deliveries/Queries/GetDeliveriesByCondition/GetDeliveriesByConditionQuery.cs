95c3fbbe2377945b381a152e43da43805480f413
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-11-01 17:34:47 +0200

    Implement GetDeliveriesByCondition query

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Features/Deliveries/Queries/GetDeliveriesByCondition/GetDeliveriesByConditionQuery.cs
@@ -0,0 +1,18 @@
+﻿using GloboDelivery.Application.Models;
+using GloboDelivery.Domain.Dtos;
+using GloboDelivery.Domain.Helpers;
+using MediatR;
+
+namespace GloboDelivery.Application.Features.Deliveries.Queries.GetDeliveriesByCondition
+{
+    public class GetDeliveriesByConditionQuery : PaginationModel, IRequest<PagedList<FullDeliveryDto>>
+    {
+        public decimal MinCapacity { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public string DepartureCountry { get; set; } = string.Empty;
+        public string DepartureCity { get; set; } = string.Empty;
+        public string ArrivalCountry { get; set; } = string.Empty;
+        public string ArrivalCity { get; set; } = string.Empty;
+    }
+}
