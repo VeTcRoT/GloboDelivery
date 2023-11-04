dbf3692876fe84f356cae900f9de9685259233ac
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-11-04 11:33:08 +0200

    Add interface of AddressValidationService

---
+++ src/API/GloboDelivery/GloboDelivery.Application/Services/Infrastructure/IAddressValidationService.cs
@@ -0,0 +1,10 @@
+﻿using GloboDelivery.Domain.Dtos;
+using SmartyStreets.USStreetApi;
+
+namespace GloboDelivery.Application.Services.Infrastructure
+{
+    public interface IAddressValidationService
+    {
+        Candidate? ValidateAddress(AddressBaseCommand address);
+    }
+}
