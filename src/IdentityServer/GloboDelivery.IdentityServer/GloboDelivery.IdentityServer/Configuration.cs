3e0a6bfe7fb8f333514d4d4829673c3c0aef6498
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-11-05 12:45:43 +0200

    Add configuration class

---
+++ src/IdentityServer/GloboDelivery.IdentityServer/GloboDelivery.IdentityServer/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace GloboDelivery.IdentityServer
+{
+    public static class Configuration
+    {
+        public static string Admin = "admin";
+        public static string Manager = "manager";
+        public static string Customer = "customer";
+    }
+}
