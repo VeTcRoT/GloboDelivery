bf87f10798900fc5c1b3d3c927cb2e96a68018f6
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-11-05 14:02:28 +0200

    Add Logout by SignInManager

--- src/IdentityServer/GloboDelivery.IdentityServer/GloboDelivery.IdentityServer/Pages/Account/Logout/Index.cshtml.cs
+++ src/IdentityServer/GloboDelivery.IdentityServer/GloboDelivery.IdentityServer/Pages/Account/Logout/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Duende.IdentityServer.Services;
 using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -15,14 +16,16 @@ public class Index : PageModel
 {
     private readonly IIdentityServerInteractionService _interaction;
     private readonly IEventService _events;
+    private readonly SignInManager<IdentityUser> _signInManager;
 
-    [BindProperty] 
+    [BindProperty]
     public string LogoutId { get; set; }
 
-    public Index(IIdentityServerInteractionService interaction, IEventService events)
+    public Index(IIdentityServerInteractionService interaction, IEventService events, SignInManager<IdentityUser> signInManager)
     {
         _interaction = interaction;
         _events = events;
+        _signInManager = signInManager;
     }
 
     public async Task<IActionResult> OnGet(string logoutId)
@@ -45,7 +48,7 @@ public class Index : PageModel
                 showLogoutPrompt = false;
             }
         }
-            
+
         if (showLogoutPrompt == false)
         {
             // if the request for logout was properly authenticated from IdentityServer, then
@@ -64,9 +67,9 @@ public class Index : PageModel
             // this captures necessary info from the current logged in user
             // this can still return null if there is no context needed
             LogoutId ??= await _interaction.CreateLogoutContextAsync();
-                
+
             // delete local authentication cookie
-            await HttpContext.SignOutAsync();
+            await _signInManager.SignOutAsync();
 
             // raise the logout event
             await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
