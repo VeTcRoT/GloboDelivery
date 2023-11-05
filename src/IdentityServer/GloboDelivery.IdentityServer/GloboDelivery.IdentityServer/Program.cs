e46d4267e81ad5a4a3bcd30ba8b189118b8b246f
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-11-05 12:35:25 +0200

    Populate services and pipeline

--- src/IdentityServer/GloboDelivery.IdentityServer/GloboDelivery.IdentityServer/Program.cs
+++ src/IdentityServer/GloboDelivery.IdentityServer/GloboDelivery.IdentityServer/Program.cs
@@ -1,6 +1,38 @@
+using GloboDelivery.IdentityServer.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
 
 app.Run();
